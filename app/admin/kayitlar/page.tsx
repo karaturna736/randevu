f00e9d7cd2cd79f3a0324b6c667422a57cac6447
karaturna736 <ahmettahais736@gmail.com>
2026-09-25 19:26:55 +0300

    Admin kayıtlar sayfasını kayıtlı üyelerle aç

--- app/admin/kayitlar/page.tsx
+++ app/admin/kayitlar/page.tsx
@@ -3,5 +3,5 @@ import Admin from "@/components/product/admin";
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return <Admin />;
+  return <Admin initialView="users" />;
 }
