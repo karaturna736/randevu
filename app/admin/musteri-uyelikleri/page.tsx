b74ee2d7200f945707993b476414e84c67e693d8
karaturna736 <ahmettahais736@gmail.com>
2026-09-26 14:58:54 +0300

    Add customer memberships admin page

---
+++ app/admin/musteri-uyelikleri/page.tsx
@@ -0,0 +1,7 @@
+import AdminCustomerMemberships from "@/components/product/admin-customer-memberships";
+
+export const dynamic = "force-dynamic";
+
+export default function Page() {
+  return <AdminCustomerMemberships />;
+}
