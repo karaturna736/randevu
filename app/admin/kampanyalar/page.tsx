fd3e480061244604a28587829ef3e33e6388b1cd
karaturna736 <ahmettahais736@gmail.com>
2026-09-26 13:55:33 +0300

    Kampanya hedef alanı düzeltmelerini admin sayfasına bağla

--- app/admin/kampanyalar/page.tsx
+++ app/admin/kampanyalar/page.tsx
@@ -1,7 +1,13 @@
 import CampaignAdmin from "@/components/product/campaign-admin";
+import CampaignTargetEnhancer from "@/components/product/campaign-target-enhancer";
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
-  return <CampaignAdmin />;
+  return (
+    <>
+      <CampaignAdmin />
+      <CampaignTargetEnhancer />
+    </>
+  );
 }
